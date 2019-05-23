bc38d308d55b99f052c4c3c0a6926866e4ee2366
Federico Franzoni <fedfranz.dev@outlook.com>
2019-05-23 16:37:49 +0200

    btc-dl DRAFT

---
+++ scripts/btc-dl.sh
@@ -0,0 +1,6 @@
+# Get URL or VERSION
+
+# If no URL or no VERSION
+# LATEST=bitcoin/bitcoin/tags/latest -- e.g. 0.18.0
+# x86bin-url=https://github.com/frz-dev/btc-x86-bin/raw/master/bin/$LATEST
+# wget $x86bin-url/bitcoind $x86bin-url/bitcoin-cli
