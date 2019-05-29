f2bda0ec1b8e84c7d62a380ad441a37877e4a8cf
Federico Franzoni <frz.dev@gmx.com>
2019-05-29 16:20:24 +0200

    ADD create-presync.sh

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Bitcoin Core node container
 
 ## Usage
 ```
-docker build -t "btcnode" .  [--build-arg btc_ver=BITCOIN_CORE_VERSION]
+docker build -t "btcnode" [--build-arg btc_ver=BITCOIN_CORE_VERSION] .
 docker run btcnode --name btcnode BITCOIND_OPTIONS
 ```
 `BITCOIN_CORE_VERSION={...,0.17.0,0.18.0}` -- All values from `frz-dev/btc-x86-bin/bin`  
@@ -29,8 +29,7 @@ All the parameters passed at the end of the `docker run` command will be used as
 docker run --name btcnode btcnode -testnet -prune=550
 
 ## Pre-Synced Node
-To save the image of a btcnode without losing data, you can commit the container
-
+To save the image of a btcnode without losing data, you can commit the container:
 ```
 # Stop Bitcoin Core
 docker exec -it btcnode bitcoin-cli -testnet stop
@@ -38,5 +37,15 @@ docker exec -it btcnode bitcoin-cli -testnet stop
 docker commit btcnode btcnode-synced
 ```
 
+### Script
+The `create-presync` script allows to create a presynced images from scratch.  
+It starts a new btcnode image, waits for it to synchronize the blockchain and then commits the image adding a -sync suffix to the image name.
+
+Usage:  
+`create-presync.sh BTCNODE_VERSION BTCOPTIONS`  
+For Example:  
+`create-presync.sh 0.17.0 -testnet -prune=550`  
+
+### Available pre-synced images
 Pre-synced images can be found in Docker `fedfranz/btcnode` repository, tagged like follows:  
 `fedfranz/btcnode:0.18.0-testnet-sync`
--- btc/create-presync.sh
+++ btc/create-presync.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 BTC_VERSION. Exiting"
+  echo "Usage: $0 BTC_VERSION BTC_OPTIONS"
   exit 1
 fi
 
 BTC_VER=$1
+BTC_OPT="${@:2}"
 BTCNODE=fedfranz/btcnode:$BTC_VER
 CNT_NAME=btcnode
-BTC_NET=-testnet
 
 # If $CNT_NAME already exists, exits
 [ "$(docker ps -a | grep $CNT_NAME)" ] && echo "Container name already exists. Exiting" && exit 1
-docker run -d --name $CNT_NAME $BTCNODE -testnet -prune=550
+docker run -d --name $CNT_NAME $BTCNODE $BTC_OPT
 if [ $? -ne 0 ]; then
     echo "Docker run failed. Exiting"; exit 1
 fi
@@ -21,19 +21,22 @@ fi
 sleep 10
 
 echo "Monitoring Initial Block Download"
-TOT=$(docker exec -it $CNT_NAME bitcoin-cli $BTC_NET getblockchaininfo | grep headers | tr -dc '0-9')
-CUR=$(docker exec -it $CNT_NAME bitcoin-cli $BTC_NET getblockcount | tr -dc '0-9')
+TOT=$(docker exec $CNT_NAME bash -c getlog | grep 'outbound peer' | tail -n 1 | grep -Po 'blocks=\K\S*' | tr -dc '0-9')
+CUR=$(docker exec $CNT_NAME bash -c getblockcount | tr -dc '0-9')
 
 while [ "$CUR" -lt "$TOT" ]; do
 echo "Blocks: $CUR/$TOT"
-T=$(( ( $TOT + $CUR ) / 10000 ))
+T=$(( ( $TOT - $CUR ) / 1000 ))
 echo "Sleeping $T seconds"
 sleep $T
-CUR=$(docker exec -it $CNT_NAME bitcoin-cli $BTC_NET getblockcount | tr -dc '0-9')
+CUR=$(docker exec -it $CNT_NAME bash -c getblockcount | tr -dc '0-9')
 done
 
+echo "$CUR/$TOT - exit"
+exit
+
 docker exec -it $CNT_NAME sync && free -m
-docker exec -it $CNT_NAME bitcoin-cli $BTC_NET stop
+docker exec -it $CNT_NAME bash -c btcstop
 while [ $(docker inspect -f '{{.State.Running}}' $CNT_NAME) ]; do sleep 1; done
 echo "Committing $CNT_NAME as $BTCNODE:sync"
 docker commit $CNT_NAME $BTCNODE-sync
